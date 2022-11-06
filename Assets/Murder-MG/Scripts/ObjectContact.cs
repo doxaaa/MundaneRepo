46343ce5931d6cca5e594cb8ad3e2cbf82166ab9
SpencerBD <spencer.dam01@gmail.com>
2022-11-06 22:40:00 +1100

    Worked on Murder Minigame

---
+++ Assets/Murder-MG/Scripts/ObjectContact.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectContact : MonoBehaviour
+{
+    public MurderManager myMurderManager;
+    
+
+    void OnTriggerEnter(Collider CollisionData)
+    {
+        if(CollisionData.name == ("cube"));
+        {
+            myMurderManager.DestroyMovingObject();
+        }
+    }
+}
