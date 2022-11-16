e103a4a8dc470a3326d06e478dbc317231498155
FUCK-YOU-LEATHE\micha <m.a.isands2002@gmail.com>
2022-11-16 14:00:49 +1100

    all of the changes/aditions that Fae made

---
+++ Assets/universal/ProtoLevelLoader.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ProtoLevelLoader : MonoBehaviour
+{
+    public GameObject victoryObject;
+    public GameObject loserObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Activate victory thing
+    public void levelWon() {
+        victoryObject.SetActive(true);
+        StartCoroutine(LoadNextLevel());
+    }
+
+    //Active lose state thing
+     public void levelLost() {
+        loserObject.SetActive(true);
+        StartCoroutine(Restart());
+    }
+
+    // Function to load the next level
+    IEnumerator LoadNextLevel()
+    {
+        yield return new WaitForSeconds(3f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    // Function to restart the game
+    IEnumerator Restart()
+    {
+        yield return new WaitForSeconds(3f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
